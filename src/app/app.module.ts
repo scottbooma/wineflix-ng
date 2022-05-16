b402cfa92c6dfb902ba8e22345ca71cfb6e44ec3
scottbooma <scottbooma@gmail.com>
2022-05-16 15:45:20 -0400

    Add nav bar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,17 @@ import {AppComponent} from './app.component';
 
 import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
 import { LogoComponent } from './logo/logo.component';
+import { NavLinksComponent } from './nav-links/nav-links.component';
+import { UserAvatarComponent } from './user-avatar/user-avatar.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LogoComponent
+    LogoComponent,
+    NavLinksComponent,
+    UserAvatarComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
